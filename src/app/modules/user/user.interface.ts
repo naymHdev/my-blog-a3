65c4cd683120e43bd37fabaf06494bac3d20289d
MacBook Pro <naym100m@gmail.com>
2024-12-19 00:02:58 +0600

    [creaet] a user model

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,13 @@
+export type TUserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type TUser = {
+  name: TUserName;
+  email: string;
+  password: string;
+  role: 'admin' | 'user';
+  isBlocked: boolean;
+};
