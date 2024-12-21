e13e92a2ceecbb7d44fa44426c9b1c565a9585a8
MacBook Pro <naym100m@gmail.com>
2024-12-21 10:04:05 +0600

    update

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -58,6 +58,9 @@ const findSingleUserFromDB = async (id: string) => {
 
 const findAllUserFromDB = async () => {
   const result = await User.find();
+
+  // console.log('result', result);
+
   return result;
 };
 
